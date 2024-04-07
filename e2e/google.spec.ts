d83705730f7ece95d983425fd866a1f350211031
Raja Durai <cr.rajadurai94@gmail.com>
2024-04-07 17:15:10 +0200

    Test feature for google

---
+++ e2e/google.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('google page open', async ({ page }) => {
+    await page.goto('https://www.google.com');
+    await expect(page.locator(`#logo`)).toBeVisible
+});
+
+test('search rea is visible', async ({ page }) => {
+    await page.goto('https://www.google.com');
+    await expect(page.locator(`textarea`)).toBeVisible
+});
+
+test('search button is present', async ({ page }) => {
+    await page.goto('https://www.google.com');
+    await expect(page.locator(`type=submit`)).toBeVisible
+});
